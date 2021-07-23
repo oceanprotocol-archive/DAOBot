3638935d0f3b01afac536c387e7dc97858bf720f
idiom-bytes <69865342+idiom-bytes@users.noreply.github.com>
2021-07-23 23:47:00 +0000

    Updating cron to log cout.

--- cron_update_standings.sh
+++ cron_update_standings.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env sh
 cd /DAOBot/
-/usr/local/bin/node src/airtable/process_airtable_all_proposal_standings.js >> log_sync_gsheets.log
+/usr/local/bin/node src/airtable/process_airtable_all_proposal_standings.js >> log_update_standings.log
