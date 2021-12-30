d5190db94eb6f892fd1e636427b1980244204268
trizin <25263018+trizin@users.noreply.github.com>
2021-12-30 15:33:07 +0300

    Use a hook to log each argument

--- src/utils/logger.js
+++ src/utils/logger.js
@@ -1,6 +1,20 @@
 const dotenv = require('dotenv')
 dotenv.config()
-const logger = require('pino')({
+const pino = require('pino')
+const hooks = {
+  logMethod(args, method) {
+    if (args.length > 1) {
+      for (let i = 1; i < args.length; i++) {
+        if (typeof args[i] === 'object')
+          args[i] = JSON.stringify(args[i], null, 2)
+        args[0] += ' %j'
+      }
+    }
+    method.apply(this, args)
+  }
+}
+const logger = pino({
+  hooks,
   level: process.env.LOG_LEVEL || 'info',
   transport: {
     target: 'pino-pretty',
